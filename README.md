b2b31a4fd885e1560b6409963ce4768fee5831f7
Salmaan Saeed <salsaeed999@gmail.com>
2025-07-10 16:00:50 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# pick_an_area_for_IT_jobs
+
+A program to pick an area to search for IT jobs
+
+![](./screenshots/program_picked_industry.PNG)
+![](./screenshots/program_picked_job_board.PNG)
+
+Create virtual environment
+```
+python -m venv venv
+```
+Activate Python virtual environment on Windows
+```
+.\venv\Scripts\Activate.ps1
+```
+
+Install dependencies
+```
+pip install -r requirements.txt
+```
---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+import pandas as pd
+from bcolors import BColors
+
+categories = ["Job_Board", "Industries"]
+category = random.choice(categories)
+
+search_message = "Search for IT jobs in ->"
+
+match category:
+    case "Job_Board":
+        df = pd.read_csv("./data/jobs_boards.csv")
+        job_boards_series = df["Job_Boards"]
+        job_boards = job_boards_series.to_list()
+        job_board = random.choice(job_boards)
+        print(search_message)
+        print("Job Board: " + BColors.LIGHT_CYAN + job_board + BColors.END)
+    case "Industries":
+        df = pd.read_csv("./data/industries.csv")
+        industries_series = df["Industries"]
+        industries = industries_series.to_list()
+        industry = random.choice(industries)
+        print(search_message)
+        print("Industry: " + BColors.LIGHT_RED + industry + BColors.END)
+    case _:
+        print("Error: category not in categories!")

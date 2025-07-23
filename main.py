3058b763ac17a71a152c127773b1cd1e04da54b2
Salmaan Saeed <salsaeed999@gmail.com>
2025-07-23 11:41:56 -0400

    Added command line arguments to specify which category to choose. Randomly pick a state for the Job_Board category. Renamed the category Industries to Industry.

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import random
 import pandas as pd
 from bcolors import BColors
+import sys
 
-categories = ["Job_Board", "Industries"]
+length = len(sys.argv)
+args = sys.argv
+category = ""
 
-# Make it so that there is a 20% chance of getting the Job_Board category
-# and a 80% chance of getting the Industries category
-sample_categories = random.sample(categories, counts=[2, 8], k = 10)
-category = random.choice(sample_categories)
+if length == 3:
+    category = args[2]
+elif length == 2:
+    print()
+    flag = args[1]
+    if (flag != "--category"):
+        print("Supply the --category flag!")
+    elif (flag == "--category"):
+        print("Supply the category for the --category flag!")
+        print("Like this:")
+        print("--category Job_Board")
+        print("--category Industry")
+    sys.exit()
+elif length == 1:
+    print("Use the program like this:")
+    print("python .\\main.py --category Job_Boards")
+    print("python .\\main.py --category Industries")
+    sys.exit()
 
 search_message = "Search for IT jobs in ->"
 
@@ -17,9 +34,15 @@ match category:
         job_boards_series = df["Job_Boards"]
         job_boards = job_boards_series.to_list()
         job_board = random.choice(job_boards)
+
+        df = pd.read_csv("./data/states.csv")
+        states_series = df["States"]
+        states = states_series.to_list()
+        state = random.choice(states)
         print(search_message)
         print("Job Board: " + BColors.LIGHT_CYAN + job_board + BColors.END)
-    case "Industries":
+        print("State: " + state)
+    case "Industry":
         df = pd.read_csv("./data/industries.csv")
         industries_series = df["Industries"]
         industries = industries_series.to_list()
